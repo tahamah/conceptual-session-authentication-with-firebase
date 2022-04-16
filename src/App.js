3cacc6b92a0afbf81964f8c45369bae4a652e0ab
unknown <taha.iu.bd@gmail.com>
2022-04-16 10:06:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Login from "./components/Auth/Login/Login";
-import RequireAuth from "./components/Auth/RequireAuth/RequireAuth";
-import Signup from "./components/Auth/Signup/Signup";
-import Generator from "./components/Generator/Generator";
-import Home from "./components/Home/Home";
-import Nav from "./components/Nav/Nav";
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Login from './components/Auth/Login/Login'
+import RequireAuth from './components/Auth/RequireAuth/RequireAuth'
+
+import Signup from './components/Auth/Signup/Signup'
+import Generator from './components/Generator/Generator'
+import Home from './components/Home/Home'
+import Nav from './components/Nav/Nav'
 
 function App() {
     return (
@@ -26,7 +27,7 @@ function App() {
                 <Route path="/signup" element={<Signup />} />
             </Routes>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
--- src/components/Auth/RequireAuth/RequireAuth.js
+++ src/components/Auth/RequireAuth/RequireAuth.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Navigate, useLocation } from 'react-router-dom';
-import { auth } from '../../../firebase.init';
+import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { Navigate, useLocation } from 'react-router-dom'
+import { auth } from '../../../firebase.init'
 
-const RequireAuth = ({children}) => {
-    const [user, loading, error] = useAuthState(auth);
-    const location = useLocation();
-    
-    if(loading){
-        return <p>Loading</p>
+const RequireAuth = ({ children }) => {
+    const [user, loading, error] = useAuthState(auth)
+    const location = useLocation()
+    if (user) {
+        return children
+    } else {
+        return <Navigate to="/login" state={{ from: location }} replace />
     }
-    
-    if(user){
-        return children;
-    } else{
-        return <Navigate to="/login" state={{from: location}} replace />
-    }
-};
+}
 
-export default RequireAuth;
+export default RequireAuth
