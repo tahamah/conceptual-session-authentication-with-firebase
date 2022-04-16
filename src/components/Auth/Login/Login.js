7fac61665615930f87aae29036077ff4c9671b52
unknown <taha.iu.bd@gmail.com>
2022-04-16 12:50:06 +0600

    pass-add

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,21 +1,40 @@
 import { useState } from 'react'
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import 'react-toastify/dist/ReactToastify.css'
 import '../../../styles/Login.css'
+import auth from '../../../firebase.init'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
+    const [errorEmail, setErrorEmail] = useState('')
+    const [errorPass, setErrorPass] = useState('')
+
+    const [signInWithEmailAndPassword, user, loading, hookError] =
+        useSignInWithEmailAndPassword(auth)
 
     const handleEmailChange = (e) => {
-        setEmail(e.target.value)
+        if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e.target.value)) {
+            setEmail(e.target.value)
+            setErrorEmail('')
+        } else {
+            setErrorEmail('Invalid Email')
+        }
     }
+    console.log(email)
     const handlePasswordChange = (e) => {
-        setPassword(e.target.value)
+        const password = e.target.value
+        if (password.length >= 6) {
+            setPassword(password)
+            setErrorPass('')
+        } else {
+            setErrorPass('minimum 6 characters')
+        }
     }
 
     const handleLogin = (e) => {
         e.preventDefault()
+        signInWithEmailAndPassword(email, password)
     }
 
     return (
@@ -27,11 +46,13 @@ const Login = () => {
                     placeholder="Your Email"
                     onChange={handleEmailChange}
                 />
+                {errorEmail && <p className="error-message">{errorEmail}</p>}
                 <input
                     type="password"
-                    placeholder="password"
+                    placeholder="Password"
                     onChange={handlePasswordChange}
                 />
+                {errorPass && <p className="error-message">{errorPass}</p>}
                 <button>Login</button>
             </form>
         </div>
